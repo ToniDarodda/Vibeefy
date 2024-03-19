64b541739be1bedb4a98b01b53672424017dfb82
ToniDarodda <toni.da.rodda.pro@gmail.com>
2024-03-19 14:55:30 -0700

    remove useless file

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const Fetch: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3002',
+  baseURL: 'http://54.164.248.243',
   timeout: 5000,
   timeoutErrorMessage:
     'Request timeout there is maybe a problem with the server!',
